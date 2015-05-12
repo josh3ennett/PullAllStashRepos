44c3f91faa57440c2fd4ee4d82a319ce5e375dee
Josh Bennett <jobennett@pointroll.com>
2015-05-12 15:10:15 -0400

    updated env setup instructions

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 [![Build Status](https://travis-ci.org/josh3ennett/PullAllStashRepos.svg?branch=master)](https://travis-ci.org/josh3ennett/PullAllStashRepos)
 
-A Command line tool to pull down all repos from stash. 
+A Command line tool to pull down all repositories from stash. 
+
+#Env Setup
+
+1. Install OpenSSL
+2. Add environment variables for the following:
+     + OPENSSL_CONF = *OpenSSL-Path*/bin/openssl.cfg  
+     + OPENSSL_INCLUDE_DIR = *OpenSSL-Path*/include  
+     + OPENSSL_LIB_DIR = *OpenSSL-Path*
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 extern crate rustc_serialize;
 extern crate docopt;
 
+use rustc_serialize::json;
+
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct HrefStruct{
     data_href: String
 }
 
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct LinksStruct{
     data_self: Vec<HrefStruct>
 }
 
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct LinkStruct{
     data_url: String,
     dataRel: String
 }
 
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct ProjectsInfoStruct  {
     data_key: String,
     data_id: u32,
@@ -25,6 +31,7 @@ pub struct ProjectsInfoStruct  {
     data_links: LinksStruct
 }
 
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct ProjectsStruct  {
     data_size: u8,
     data_limit: u8,
