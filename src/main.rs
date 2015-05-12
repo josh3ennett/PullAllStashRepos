8f6ec9bc9c3bbac17d90e9937c9d2d90d146dd88
Josh Bennett <jobennett@pointroll.com>
2015-05-12 14:55:06 -0400

    starting to setup structs for json objects

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
 extern crate rustc_serialize;
 extern crate docopt;
 
+pub struct HrefStruct{
+    data_href: String
+}
+
+pub struct LinksStruct{
+    data_self: Vec<HrefStruct>
+}
+
+pub struct LinkStruct{
+    data_url: String,
+    dataRel: String
+}
+
+pub struct ProjectsInfoStruct  {
+    data_key: String,
+    data_id: u32,
+    data_name: String,
+    data_description: String,
+    data_public: bool,
+    data_type: String,
+    data_link: LinkStruct,
+    data_links: LinksStruct
+}
+
+pub struct ProjectsStruct  {
+    data_size: u8,
+    data_limit: u8,
+    data_vector: bool,
+    data_values: Vec<ProjectsInfoStruct>
+}
+
 fn main() {
     println!("Hello, world!");
 }
