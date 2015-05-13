fec87d0e8b9fec4955e61ad38d47a577600225c5
Josh Bennett <jobennett@pointroll.com>
2015-05-12 22:44:21 -0400

    basic read from endpoint

--- README.md
+++ README.md
@@ -4,10 +4,14 @@
 
 A Command line tool to pull down all repositories from stash. 
 
-#Env Setup
+##Windows Development Environment Setup
 
 1. Install OpenSSL
 2. Add environment variables for the following:
      + OPENSSL_CONF = *OpenSSL-Path*/bin/openssl.cfg  
      + OPENSSL_INCLUDE_DIR = *OpenSSL-Path*/include  
      + OPENSSL_LIB_DIR = *OpenSSL-Path*
+
+##Quick Links
+
++ [Stash Command Line Rest](https://developer.atlassian.com/stash/docs/latest/how-tos/command-line-rest.html)
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-extern crate rustc_serialize;
 extern crate docopt;
+extern crate hyper;
+extern crate rustc_serialize;
 
+use docopt::Docopt;
+use hyper::Client;
+use hyper::client::Request;
+use hyper::Url;
 use rustc_serialize::json;
+use std::io::Read;
 
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct HrefStruct{
@@ -20,7 +26,7 @@ pub struct LinkStruct{
 }
 
 #[derive(RustcDecodable, RustcEncodable)]
-pub struct ProjectsInfoStruct  {
+pub struct ProjectInfoStruct  {
     data_key: String,
     data_id: u32,
     data_name: String,
@@ -36,9 +42,43 @@ pub struct ProjectsStruct  {
     data_size: u8,
     data_limit: u8,
     data_vector: bool,
-    data_values: Vec<ProjectsInfoStruct>
+    data_values: Vec<ProjectInfoStruct>
 }
 
+#[derive(RustcDecodable, Debug)]
+struct Args {
+    arg_url: String,
+    arg_username: String,
+    arg_outdir: String,
+    flag_verbos: bool
+}
+static USAGE: &'static str = "
+Usage: PullAllStashRepos [Options] <url> <outdir> <username>
+
+Options:
+    -v, --verbos  show everything.
+";
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    println!("{:?}", args);
+
+    let mut client = Client::new();
+    let mut urlStr = args.arg_url.to_string();
+    urlStr = urlStr + "/rest/api/1.0/projects/";
+
+    let url = Url::parse(&*urlStr).unwrap();
+
+    // TODO use basic auth
+    let mut res = client.get(url).send().unwrap();
+
+    let mut bodyText = String::new();
+
+    &res.read_to_string(&mut bodyText);
+
+    println!("{:?}", bodyText);
 }
